357956d9e54bd71cfbc0eb7123ad7a4f960bfe58
Pedro Alves <pedro22carro@gmail.com>
2020-11-19 19:26:47 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Array");
+
+            string[] nomes = new string[3];
+            int[] idades = new int[3];
+
+            for (var contador = 0; contador < 3; contador++)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine($"Digite o {contador+1}º nome");
+                nomes[contador] = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Digite a idade");
+                idades[contador] = int.Parse(Console.ReadLine());
+
+            }
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Nomes Cadastrados:");
+
+            for (var contador = 0; contador < 3; contador++)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Nome: "+nomes[contador]);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Idade: "+idades[contador]);
+            }
+                Console.ResetColor();
+        }
+    }
+}
